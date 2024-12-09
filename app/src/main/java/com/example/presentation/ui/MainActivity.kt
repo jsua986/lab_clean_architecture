1259f09f29e2cb23e3e96fcc74acbe60667d8690
josua ortiz <Josua.Ortiz@popular.com>
2024-12-09 15:14:21 -0600

    first commit

---
+++ app/src/main/java/com/example/presentation/ui/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.presentation.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.templatehiltapp.ui.theme.TemplateHiltAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TemplateHiltAppTheme {
+                Surface() {
+                UserProfileApp()
+            }
+            }
+        }
+    }
+}
